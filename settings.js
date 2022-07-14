7487cd0f8fb7c311eef3e5efaca58f20b7b40d0b
rain4you <105019762+rain4you@users.noreply.github.com>
2022-07-14 20:32:02 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,31 +19,30 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `Rainbetmutxyz` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+6282274499424', 'Rainbetmutxyz'] //Owner number in aive msg
+global.premium = ['+6282274499424'] //Owner Number info
+global.ownernomer = '+6282274499424, 'Rainbetmutxyz' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = 'Rainbetmutxyz' //Owner Name
+global.botname = ' Rainbetmutxyz`' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.button = 'Tiktok' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://vt.tiktok.com/ZSRd7dJ37/' //Change Your Button's URL
 
-global.region = 'India, South India, Kerala' //Country Region
+global.region = 'Indonesia' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'Rainbetmutxyz' //Bot Pacage Name
+global.author = 'Rainbetmutxyz' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '© Rainbetmutxyz' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://github.com/rain4you' //Github Account!
 
 global.mess = { // cOSTOMIZE WITH YOUR LANGUAGE
 	
